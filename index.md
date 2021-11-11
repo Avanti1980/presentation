cd3f1a2c4082b14c55836683203a8d34f2d9a3de
murongxixi <murongxixi@gmail.com>
2021-11-11 23:47:32 +0800

    update

---
+++ index.md
@@ -0,0 +1,4 @@
+@import "common/css/font-awesome-4.7.0/css/font-awesome.css"
+@import "common/css/index.css"
+
+[211112 算法导论一道例题讲解](211112-alg/211112-alg.html)
---
+++ upload.sh
@@ -0,0 +1,18 @@
+git add *
+git commit -m $1
+
+case $2 in
+"ee")
+    git push gitee master
+    ;;
+"hub")
+    git push github master
+    ;;
+"both")
+    git push gitee master
+    git push github master
+    ;;
+*)
+    echo "error: 2nd par must be [ee|hub|both]!"
+    ;;
+esac
